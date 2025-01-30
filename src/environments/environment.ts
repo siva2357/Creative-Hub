a25755aca1ee66d4a6756c11423e894c45618210
Siva2357 <sivakurra.ksp2357@gmail.com>
2025-01-30 15:50:34 +0530

    refactroed environment code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,28 +1,16 @@
-// export const environment = {
-//   production: false,
-//   firebaseConfig: {
-//     apiKey: "your-api-key",
-//     authDomain: "your-project.firebaseapp.com",
-//     projectId: "your-project-id",
-//     storageBucket: "your-project.appspot.com",
-//     messagingSenderId: "your-messaging-sender-id",
-//     appId: "your-app-id"
-//   }
-// };
-
-
 export const environment = {
   production: false,
-  firebaseConfig : {
-    apiKey: "AIzaSyBGU2-I-XrRTOqvqLV6XWubGmT2y_ZQgKs",
-    authDomain: "core-feat-446514-j7.firebaseapp.com",
-    projectId: "core-feat-446514-j7",
-    storageBucket: "core-feat-446514-j7.firebasestorage.app",
-    messagingSenderId: "71682140362",
-    appId: "1:71682140362:web:373b312092eba754baec33",
-    measurementId: "G-VY7PR82NS0"
+  firebaseConfig: {
+    apiKey: "your-api-key",
+    authDomain: "your-project.firebaseapp.com",
+    projectId: "your-project-id",
+    storageBucket: "your-project.appspot.com",
+    messagingSenderId: "your-messaging-sender-id",
+    appId: "your-app-id"
   }
 };
+
+
 
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
