caa7afd00b62760dd43fbf3e36704317134beb08
Siva2357 <sivakurra.ksp2357@gmail.com>
2025-02-03 15:15:10 +0530

    Added  register company and university forms for storing data for respective pages

--- src/app/Front-End/views/Pages/talent-page/admin/university/university.component.ts
+++ src/app/Front-End/views/Pages/talent-page/admin/university/university.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,12 +7,46 @@ import { Router } from '@angular/router';
   templateUrl: './university.component.html',
   styleUrls: ['./university.component.css']
 })
-export class UniversityComponent {
 
-       constructor(private router: Router) { }
+export class UniversityComponent implements OnInit{
 
-       goToUniversityDetails(): void {
-            this.router.navigateByUrl('talent-page/admin/university-details/:id');
-          }
+  logoUploadUrl: string | ArrayBuffer | null = null;
 
+  universityPostForm!: FormGroup;
+  errorMessage: string = '';
+  jobCreated:boolean=false;
+  isLoading:boolean=false;
+  isSubmitting:boolean=false;
+
+
+  constructor(private fb: FormBuilder,private router: Router ) {}
+
+  ngOnInit(): void{
+
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.universityPostForm = this.fb.group({
+      _id: [null],
+      universityId: ['', [Validators.required]],
+      universityLogo: ['', [Validators.required]],
+      universityName: ['', [Validators.required]],
+      universityType: ['', [Validators.required]],
+      universityWebsite: ['', [Validators.required]],
+      universityDescription: ['', [Validators.required]],
+      universityProgram: ['', [Validators.required]],
+      universityCourse: ['', [Validators.required]],
+    });
+  }
+
+
+
+  submitUniversity() {
+  }
+
+  
+  goToUniversityDetails(): void {
+    this.router.navigateByUrl('talent-page/admin/university-details/:id');
+  }
 }
